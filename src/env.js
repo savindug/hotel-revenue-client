e0df404868e76dc419b64e545e9cff57771c4b23
Savindu Gunawardena <savibashnon11@gmail.com>
2021-04-26 22:04:38 +0530

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,3 +1,3 @@
 //export const apiURI = 'http://localhost:5000/api/';
-//export const apiURI = 'https://98645cd54ed6.ngrok.io/api/';
-export const apiURI = 'https://api.ratebuckets.com/api/';
+export const apiURI = 'https://821961594dfb.ngrok.io/api/';
+//export const apiURI = 'https://api.ratebuckets.com/api/';
