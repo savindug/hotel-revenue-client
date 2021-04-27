3525d93fbfb22b3b6d3c4561d4bd42f929c76a36
Savindu Gunawardena <savibashnon11@gmail.com>
2021-04-27 14:14:31 +0530

    updated service url

--- src/env.js
+++ src/env.js
@@ -1,3 +1,3 @@
 //export const apiURI = 'http://localhost:5000/api/';
-export const apiURI = 'https://932c786efc0e.ngrok.io/api/';
+export const apiURI = 'https://e25eaa7b118d.ngrok.io/api/';
 //export const apiURI = 'https://api.ratebuckets.com/api/';
