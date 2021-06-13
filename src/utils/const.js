ed15665f64efe57dc82d33273b4c803ac355faa0
Savindu Gunawardena <savibashnon11@gmail.com>
2021-06-13 22:46:19 +0530

    change bucket colrs

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,4 +1,4 @@
 export const AUTHORIZATION_KEY = 'AUTHORIZATION';
 export const REFRESH_KEY = 'REFRESH_KEY';
 
-export const CLUSTER_BACKGROUND = ['#A358DF', '#FB275D', '#009AFF', '#00CC6F'];
+export const CLUSTER_BACKGROUND = ['#009AFF', '#FFCC00', '#FB275D', '#00CC6F'];
