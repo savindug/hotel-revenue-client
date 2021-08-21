802f73c17cf71a18eb3c667a11af9156d0d0e571
Savindu Gunawardena <savibashnon11@gmail.com>
2021-08-21 22:12:43 +0530

    update serice url

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 //export const apiURI = 'http://localhost:5000/api/';
-export const apiURI = 'https://85010bb30375.ngrok.io/api/';
+export const apiURI = 'https://service.ratebuckets.com/api/';
 // export const apiURI = 'https://api.ratebuckets.com/api/';
 
 export const GOOGLE_MAP_KEY = 'AIzaSyCzDd8gDOD8VTT40i6J8wl543L6sxLv8L8';
