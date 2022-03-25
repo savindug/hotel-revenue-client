1d28f1e1032287cb4eb66ad1e49eff8e1091ce40
savindug <savibashnon11@gmail.com>
2022-03-25 22:06:55 +0530

    fix the rating freq

--- src/components/HotelsPlot.js
+++ src/components/HotelsPlot.js
@@ -15,7 +15,6 @@ export function HotelsPlot({ hotels, dateSelection }) {
     cluster3,
     cluster4,
     reqHotel,
-    report_len,
   } = getClusterDataSet;
 
   const [plotData, setPlotData] = useState([]);
@@ -31,6 +30,8 @@ export function HotelsPlot({ hotels, dateSelection }) {
   const [scatterPlot, setScatterPlot] = useState(2);
 
   const [scatterPlotLabels, setScatterPlotLabels] = useState();
+
+  const report_len = 90;
 
   const getClusterByPrice = (rate, ix) => {
     let clustered = [];
@@ -74,25 +75,31 @@ export function HotelsPlot({ hotels, dateSelection }) {
       .pop();
   };
 
-  const getFreqBucketMinMaxRatings = (dt, bucket) => {
+  const getFreqBucketMinMaxRatings = (bucket) => {
     // console.log(`dt: ${dt}, bucket: ${bucket}`);
     let min = -1;
     let max = -1;
 
-    if (clusterData[dt][bucket]) {
-      if (clusterData[dt][bucket].unwanted.length > 0) {
-        let hotelsArr = clusterData[dt][bucket].unwanted;
+    let hotelsArr = [];
 
-        min = Math.min.apply(
-          null,
-          hotelsArr.map((item) => item.raings)
-        );
-        max = Math.max.apply(
-          null,
-          hotelsArr.map((item) => item.raings)
-        );
-      }
+    if (clusterData.length > 0) {
+      [...Array(report_len).keys()].map((ob, dt) => {
+        if (clusterData[dt][bucket]) {
+          if (clusterData[dt][bucket].unwanted.length > 0) {
+            hotelsArr = [...hotelsArr, ...clusterData[dt][bucket].unwanted];
+          }
+        }
+      });
     }
+    // console.log(`hotelsArr.length = ${hotelsArr.length}`);
+    min = Math.min.apply(
+      null,
+      hotelsArr.map((item) => item.raings)
+    );
+    max = Math.max.apply(
+      null,
+      hotelsArr.map((item) => item.raings)
+    );
     return { min: min, max: max };
   };
 
@@ -129,31 +136,43 @@ export function HotelsPlot({ hotels, dateSelection }) {
             }
           });
           _hotel.freq_bucket = mode(cluster_arr);
+          const FrqBucketMinMax = getFreqBucketMinMaxRatings(
+            _hotel.freq_bucket - 2
+          );
+
+          _hotel.FrqBucketMinMax = FrqBucketMinMax;
+          if (FrqBucketMinMax.min > 0 && FrqBucketMinMax.max > 0) {
+            _hotel.frq_rating = (
+              Math.abs(_hotel.ratings - _hotel.freq_bucket) /
+                (FrqBucketMinMax.max - FrqBucketMinMax.min) +
+              _hotel.freq_bucket
+            ).toFixed(2);
+          }
         });
 
-        [...Array(report_len).keys()].map((ob, idx) => {
-          hotels.map((_hotel) => {
-            if (
-              _hotel.prices[idx] !== null &&
-              _hotel.prices[idx] !== undefined
-            ) {
-              const FrqBucketMinMax = getFreqBucketMinMaxRatings(
-                idx,
-                _hotel.freq_bucket - 2
-              );
+        // [...Array(report_len).keys()].map((ob, idx) => {
+        //   hotels.map((_hotel) => {
+        //     if (
+        //       _hotel.prices[idx] !== null &&
+        //       _hotel.prices[idx] !== undefined
+        //     ) {
+        //       // const FrqBucketMinMax = getFreqBucketMinMaxRatings(
+        //       //   idx,
+        //       //   _hotel.freq_bucket - 2
+        //       // );
 
-              _hotel.prices[idx].FrqBucketMinMax = FrqBucketMinMax;
+        //       // _hotel.prices[idx].FrqBucketMinMax = FrqBucketMinMax;
 
-              if (FrqBucketMinMax.min > 0 && FrqBucketMinMax.max > 0) {
-                _hotel.prices[idx].frq_rating = (
-                  Math.abs(_hotel.ratings - _hotel.freq_bucket) /
-                    (FrqBucketMinMax.max - FrqBucketMinMax.min) +
-                  _hotel.freq_bucket
-                ).toFixed(2);
-              }
-            }
-          });
-        });
+        //       // if (FrqBucketMinMax.min > 0 && FrqBucketMinMax.max > 0) {
+        //       //   _hotel.prices[idx].frq_rating = (
+        //       //     Math.abs(_hotel.ratings - _hotel.freq_bucket) /
+        //       //       (FrqBucketMinMax.max - FrqBucketMinMax.min) +
+        //       //     _hotel.freq_bucket
+        //       //   ).toFixed(2);
+        //       // }
+        //     }
+        //   });
+        // });
 
         setHotelsDataPlot(hotels);
 
@@ -177,10 +196,13 @@ export function HotelsPlot({ hotels, dateSelection }) {
                   getReqHotelData().localeCompare(_hotel.hotelName) == 0
                     ? 6
                     : 3,
-                backgroundColor: '#2e2e2e',
+                backgroundColor:
+                  getReqHotelData().localeCompare(_hotel.hotelName) == 0
+                    ? '#516B8F'
+                    : '#2e2e2e',
                 data: [
                   {
-                    x: _hotel.prices[dateSelection].frq_rating,
+                    x: _hotel.frq_rating,
                     y: _hotel.prices[dateSelection].price[
                       getPrice(_hotel.prices[dateSelection].price)
                     ],
@@ -199,6 +221,7 @@ export function HotelsPlot({ hotels, dateSelection }) {
     if (hotels.length > 0 && clusterData.length > 0) {
       buildPlot();
       getReqHotelData();
+      // console.log(hotels);
     }
   }, []);
 
@@ -220,10 +243,13 @@ export function HotelsPlot({ hotels, dateSelection }) {
                 getReqHotelData().localeCompare(_hotel.hotelName) == 0 ? 6 : 3,
               pointHoverRadius:
                 getReqHotelData().localeCompare(_hotel.hotelName) == 0 ? 6 : 3,
-              backgroundColor: '#2e2e2e',
+              backgroundColor:
+                getReqHotelData().localeCompare(_hotel.hotelName) == 0
+                  ? '#516B8F'
+                  : '#2e2e2e',
               data: [
                 {
-                  x: _hotel.prices[dateSelection].frq_rating,
+                  x: _hotel.frq_rating,
                   y: _hotel.prices[dateSelection].price[
                     getPrice(_hotel.prices[dateSelection].price)
                   ],
@@ -293,7 +319,6 @@ export function HotelsPlot({ hotels, dateSelection }) {
       ) : (
         <></>
       )}
-      1
     </>
   );
 }
